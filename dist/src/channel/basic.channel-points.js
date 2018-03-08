4cb271a34c0cac20fd6c77698181a2ac7b211c05
Raven <rdev1163@outlook.com>
2018-03-07 17:50:17 -0800

    cleaning files

--- dist/src/channel/basic.channel-points.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=basic.channel-points.js.map
--- dist/src/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var service_1 = require("./service");
-function boot() {
-    service_1.default.bootstrap();
-}
-boot();
-//# sourceMappingURL=main.js.map
